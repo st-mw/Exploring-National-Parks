5a3f5eb48240c8584dcf1aa83ee41644084004fc
st-mw <118785603+st-mw@users.noreply.github.com>
2024-10-29 22:52:33 -0400

    Add button to go to a random live park webcam

--- exploring-national-parks/src/GlobalComponents/Navbar.jsx
+++ exploring-national-parks/src/GlobalComponents/Navbar.jsx
@@ -9,7 +9,20 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import '../Style/navbar.css'
 import tree from './tree.png'
+
 const Navbar = () => {
+
+    let index = Math.floor(Math.random() * 4);
+
+    const activeCams = ["https://www.nps.gov/chis/learn/photosmultimedia/ocean-webcam.htm", 
+        "https://www.nps.gov/chis/learn/photosmultimedia/bald-eagle-webcam.htm", 
+        "https://www.nps.gov/media/webcam/view.htm?id=81B4690A-1DD8-B71B-0B2D069E1CB02E6A",
+        "https://www.nps.gov/media/webcam/view.htm?id=81B46955-1DD8-B71B-0B698C4D88410C05" 
+    ]
+
+    const link = `${activeCams[index]}`
+
+
     return (
         <nav className="nav-bar">
             <ul>
@@ -30,6 +43,9 @@ const Navbar = () => {
                 </li>
                 <li>
                     <NavLink to="/ParkPlan">Park Planner</NavLink>
+                </li>
+                <li>
+                    <NavLink to={link}>Random Live Webcam</NavLink>
                 </li>
             </ul>
         </nav>
