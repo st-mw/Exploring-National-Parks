8453b5010b58475080f064c22611bbe981b555ba
st-mw <118785603+st-mw@users.noreply.github.com>
2024-10-29 21:59:04 -0400

    Edit homepage to display news feature

--- exploring-national-parks/src/HomePage.js
+++ exploring-national-parks/src/HomePage.js
@@ -10,14 +10,16 @@ import Buttons from './HomePage/Components/Buttons'
 import yosemite from './HomePage/Assets/yosemite.jpg';
 import './Style/homepage.css'
 import HighlightGallery from './HomePage/Components/HighlightGallery';
+import RecentArticles from './HomePage/Components/RecentArticles';
+
 const HomePage = () => {
   return (
     // <Navbar/>
     <div className = "home-page main-component">
-        {/* <h1>Test Hello</h1> */}
         <Welcome/>
         <HighlightGallery/>
         <Buttons/>
+        <RecentArticles/>
     </div>
   )
 }
